9d4b675cc9faf7908779383c45ee7a0559435108
MKody <im@kdy.ch>
2015-08-12 19:47:52 +0000

    Update, no /shorten because it's broken

--- README.md
+++ README.md
@@ -20,20 +20,20 @@ Requires PHP ≥ 5.4.0 or higher.
 * Doesn’t create multiple short URLs when you try to shorten the same URL. In this case, the script will simply return the existing short URL for that long URL.
 * DRY, minimal code.
 * Correct, semantic use of the available HTTP status codes.
-* Can be used with Twitter for iPhone. Just go to _Settings_ › _Services_ › _URL Shortening_ › _Custom…_ and enter `http://yourshortener.ext/shorten?url=%@`.
+* Can be used with Twitter for iPhone. Just go to _Settings_ › _Services_ › _URL Shortening_ › _Custom…_ and enter `http://yourshortener.ext/shorten.php?url=%@`.
 
 ## Favelets / Bookmarklets
 
 ### Prompt
 
 ``` js
-javascript:(function(){var%20q=prompt('URL:');if(q){document.location='http://yourshortener.ext/shorten?url='+encodeURIComponent(q)}}());
+javascript:(function(){var%20q=prompt('URL:');if(q){document.location='http://yourshortener.ext/shorten.php?url='+encodeURIComponent(q)}}());
 ```
 
 ### Shorten this URL
 
 ``` js
-javascript:(function(){document.location='http://yourshortener.ext/shorten?url='+encodeURIComponent(location.href)}());
+javascript:(function(){document.location='http://yourshortener.ext/shorten.php?url='+encodeURIComponent(location.href)}());
 ````
 
 ## License
--- add/index.php
+++ add/index.php
@@ -1,20 +1,19 @@
 <!doctype html>
 <html>
   <head>
+    <title>Shorten the URL</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>URL Shortner</title>
-
     <!-- load MUI -->
     <link href="//cdn.muicss.com/mui-0.1.18/css/mui.min.css" rel="stylesheet" type="text/css" />
     <script src="//cdn.muicss.com/mui-0.1.18/js/mui.min.js"></script>
+    <script src="//code.jquery.com/jquery-latest.js"></script> <!-- load jquery via CDN -->
+
     <style>
       .highlight {background: #F8F8F8 none repeat scroll 0% 0%;}
       .highlight pre {margin-top: -10px;}
     </style>
-
-    <script src="//code.jquery.com/jquery-latest.js"></script> <!-- load jquery via CDN -->
     <script>
         $(document).ready(function() {//start document ready
             $('.mui-btn').click(function (e){
@@ -49,7 +48,7 @@
           <button type="submit" class="mui-btn mui-btn-default mui-btn-raised">Submit</button>
         </form>
         <form class="mui-form-inline">
-         <input type="text" class="mui-form-control" id="result">
+         <input type="text" class="mui-form-control" id="result" value="http://<?php echo $_SERVER['HTTP_HOST']; ?>/">
         </form>
       </div>
       <div class="mui-panel">
@@ -57,11 +56,11 @@
         <p>Replace <code><u>[url]</u></code> with your long URL.</p>
         <p>
           Returns the short URL from the domain:
-          <div class="highlight"><pre><code>http://<?php echo $_SERVER['HTTP_HOST']; ?>/shorten?url=<u>[url]</u></code></pre></div>
+          <div class="highlight"><pre><code>http://<?php echo $_SERVER['HTTP_HOST']; ?>/shorten.php?url=<u>[url]</u></code></pre></div>
         </p>
         <p>
           Returns only the slug:
-          <div class="highlight"><pre><code>http://<?php echo $_SERVER['HTTP_HOST']; ?>/shorten?<b>s&</b>url=<u>[url]</u></code></pre></div>
+          <div class="highlight"><pre><code>http://<?php echo $_SERVER['HTTP_HOST']; ?>/shorten.php?<b>s&</b>url=<u>[url]</u></code></pre></div>
         </p>
       </div>
     </div>
